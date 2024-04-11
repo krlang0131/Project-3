957c39bcef871c72afdbaa4154fbc66a02d8158a
krlang0131 <krlang0131@gmail.com>
2024-04-10 21:58:05 -0400

    added js and adjusted to function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// alert("it works");
+
+
+$(".play-button").click( function() {
+    $("#interact-box").removeClass();
+    $("#interact-box").toggleClass("show-spring");
+  })
+$(".spring-off").click( function() {
+    $("#interact-box").removeClass();
+    $("#interact-box").toggleClass("show-spring");
+  })
+$(".summer-off").click( function() {
+    $("#interact-box").removeClass();
+    $("#interact-box").toggleClass("show-summer");
+  })
+$(".fall-off").click( function() {
+    $("#interact-box").removeClass();
+    $("#interact-box").toggleClass("show-fall");
+  })
+$(".winter-off").click( function() {
+    $("#interact-box").removeClass();
+    $("#interact-box").toggleClass("show-winter");
+  })
